493bece161220e25a9fc38b77f577abf6de9606c
Artyom V. Peredery <artperedery@gmail.com>
2021-04-22 20:42:46 +0300

    feat: complete fp2
    fix my_compact
    
    fix
    
    fix block

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -5,16 +5,32 @@ module Exercise
       # Использовать свои написанные функции для реализации следующих - можно.
 
       # Написать свою функцию my_each
-      def my_each; end
+      def my_each(&func)
+        e, *rest = self
+        func.call(e)
+        rest = MyArray.new(rest)
+        rest.my_each(&func) if rest.any?
+        self
+      end
 
       # Написать свою функцию my_map
-      def my_map; end
+      def my_map
+        my_reduce(MyArray.new) { |acc, e| acc << yield(e) }
+      end
 
       # Написать свою функцию my_compact
-      def my_compact; end
+      def my_compact
+        my_reduce(MyArray.new) { |acc, e| e.nil? ? acc : acc << e }
+      end
 
       # Написать свою функцию my_reduce
-      def my_reduce; end
+      def my_reduce(acc = nil, &func)
+        e, *rest = self
+        acc = acc.nil? ? e : func.call(acc, e)
+        return acc if rest.empty?
+
+        MyArray.new(rest).my_reduce(acc, &func)
+      end
     end
   end
 end
